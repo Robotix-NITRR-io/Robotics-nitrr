21fb18f0d796965e710d94d763a49b40e6136e6a
AmanDhananjay <112410719+AmanDhananjay@users.noreply.github.com>
2023-03-06 18:09:25 +0530

    go

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
   
 export const Nav = styled.nav`
-  background: #061691;
+  background: #0412d9;
   height: 85px;
   display: flex;
   justify-content: space-between;
